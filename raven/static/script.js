78027aeff037ff8abb7ddf556d2bdb270f1649d4
Myles Braithwaite <me@mylesbraithwaite.com>
2016-09-02 12:41:54 -0400

    Got the JavaScript working.

--- raven/static/script.js
+++ raven/static/script.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
-    $('#js-contact-form').on('submit', function(e) {
+    var form = $('#js-contact-form'),
+        error_alert = $('#js-contact-form-error'),
+        sucess_alert = $('#js-contact-form-sucess');
+
+    form.on('submit', function(e) {
         e.preventDefault();
 
-        
+        if (form.validate()) {
+            $.ajax({
+                type: 'POST',
+                url: form.attr('action'),
+                data: form.serialize(),
+                success: function(data) {
+                    if (data['sent'] === 'ok') {
+                        form.hide();
+                        sucess_alert.show();
+                    } else {
+                        form.hide();
+                        error_alert.show();
+                    }
+                },
+                error: function(data) {
+                    form.hide();
+                    error_alert.show();
+                }
+            });
+        }
     });
 });
