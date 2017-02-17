b7b75f64cdc0462e3bdaf21c582aed8ff84f663a
Myles Braithwaite <me@mylesbraithwaite.com>
2017-02-17 13:03:56 -0500

    :wrench: Fixing the configuration.

--- raven/config.py
+++ raven/config.py
@@ -5,6 +5,8 @@ import json
 class Config(object):
     DEBUG = True
 
+    SECRET_KEY = os.environ.get('RAVEN_SECRET_KEY', 'secret-key')
+
     CORS_ORIGINS = os.environ.get('RAVEN_CORS_ORIGINS', '*')
     CORS_RESOURCES = r'/api/*'
 
@@ -18,6 +20,12 @@ class Config(object):
     RECAPTCHA_SITE_KEY = os.environ.get('RAVEN_RECAPTCHA_SITE_KEY')
     RECAPTCHA_SECRET_KEY = os.environ.get('RAVEN_RECAPTCHA_SECRET_KEY')
     RECAPTCHA_TYPE = 'image'
+
+    CLEARBIT_KEY = os.environ.get('RAVEN_CLEARBIT_KEY')
+
+    OPBEAT_ORGANIZATION_ID = os.environ.get('RAVEN_OPBEAT_ORGANIZATION_ID')
+    OPBEAT_APP_ID = os.environ.get('RAVEN_OPBEAT_APP_ID')
+    OPBEAT_SECRET_TOKEN = os.environ.get('RAVEN_OPBEAT_SECRET_TOKEN')
 
     with open('config.json', 'r') as fobj:
         RAVEN_CONFIG = json.loads(fobj.read())
