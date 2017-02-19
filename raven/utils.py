1b2905ad705826e329b39957cf12bae3bb5376fc
Myles Braithwaite <me@mylesbraithwaite.com>
2017-02-19 15:22:48 -0500

    :rotating_light: Fixing a small lint issue.

--- raven/utils.py
+++ raven/utils.py
@@ -2,6 +2,7 @@ import clearbit
 
 from flask import current_app
 
+
 def get_clearbit_data(email):
     clearbit.key = current_app.config['CLEARBIT_KEY']
     return clearbit.Enrichment.find(email=email, stream=True)
